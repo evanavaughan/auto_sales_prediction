3bc42391b54dbc1b23c386291059c85bf94e4527
Evan <evan13@gmail.com>
2019-05-07 11:53:16 -0400

    object oriented scraper

--- scraper.py
+++ scraper.py
@@ -1,7 +1,10 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
-class Scraper:
+class Scraper2:
+    site = 'http://www.goodcarbadcar.net/2019/05/april-2019-the-best-selling-vehicles-in-america-every-vehicle-ranked/'
+
     def webpage_html(self, url):
         nyc_request = requests.get(url)
         self.timeout_html = nyc_request.text
@@ -10,11 +13,19 @@ class Scraper:
     def listing_page(self, timeout_html = None):
         timeout_html = timeout_html or self.timeout_html
         timeout_soup = BeautifulSoup(timeout_html)
-        individual_listing =  timeout_soup.findAll('div', {'class': ' non_hotels_like desktop scopedSearch'})
+        individual_listing =  timeout_soup.find('tbody')
         self.individual_listing = individual_listing
         return self.individual_listing
 
-    def scrape(self, url):
-        website = requests.get(url)
-        self.auto_data = website.text
-        return self.auto_data
+
+class Parser:
+    def __init__(self, listing_page):
+        self.listing_page = listing_page
+
+    def car_sales(self):
+        monthly_sales = []
+        table = self.listing_page.findAll('tr', {'id':re.compile('^table_3*')})
+        for i in range(0, len(table)-1):
+            row = self.table[i].text.split('\n')
+            monthly_sales.append(self.row)
+        return self.monthly_sales
