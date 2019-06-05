421da2620af54a7c366ec073b437d127ac38fbfa
Evan <evan13@gmail.com>
2019-06-05 11:42:12 -0400

    moved files

--- scrapers/kbb_scraper.py
+++ scrapers/kbb_scraper.py
@@ -5,26 +5,17 @@ import csv
 import re
 from variables import search_results, pre, suf
 
-def clean(url):
-    '''
-    felper function for string cleaning the individual car page urls
-    '''
-    url = url.replace('staging.', '')
-    url = url.replace('//', '//www.')
-    separator = '?'
-    return url.split(separator, 1)[0]
 
-class ListingBuilder:
-    pass
+
 
 
 
 class Scraper:
     '''
     bob = Scraper()
-    bob.get_listing_pages()
+    bob.get_listing_pages(pre, suf)
     bob.clean_url_list()
-    bob.write_list_to_file('./urls/url_list.csv')
+    bob.backup_list_to_csv('./urls/url_list.csv')
     '''
 
     def get_lxml(self, site):
@@ -32,16 +23,11 @@ class Scraper:
         self.site_lxml = BeautifulSoup(html, 'lxml')
         return self.site_lxml
 
-    def get_listing_pages(self):
+    def get_listing_pages(self, prefix, suffix):
         '''scrapes search results to get url list for individual car models'''
-
-        prefix = 'https://www.kbb.com/car-finder/'
-        suffix = '/?categories=sedan&years=2006-2020'
         self.urls = []
         for i in range(1,5):        #change to 139
             site  = prefix + str(i) + suffix
-            #html = requests.get(site).text
-            #soup = BeautifulSoup(html, 'lxml')
             for link in self.get_lxml(site).find_all('a'):
                 self.urls.append(link.get('href'))
         return self.urls
@@ -59,10 +45,135 @@ class Scraper:
         return self.cleaned_url_list
 
 
-    def write_list_to_file(self, filename): #save to './urls/url_list.csv'
+    def backup_list_to_csv(self, filename):
         '''write the list to csv file'''
         urls = self.cleaned_url_list
         with open(filename, "w") as outfile:
             for row in urls:
                 outfile.write(row)
                 outfile.write("\n")
+
+
+class ModelParser:
+
+    def __init__(self, site_lxml):
+        self.model_page = site_lxml
+
+    def combined_mpg(self):
+        '''extracts and returns the combined mpg'''
+
+        combined = self.model_page.find('p', {'class':'label-two'}).text
+        combined = int(combined.split()[0])
+        return combined
+
+    def city_hwy_mpg(self):
+        '''extracts and returns the city and hwy mpg'''
+
+        mpg = self.model_page.find('p', {'class':'label-three'}).text.split()
+        city, hwy = int(mpg[0]), int(mpg[3])
+        return city, hwy
+
+    def ratings(self):
+        '''extracts and expert and user ratings from page as float'''
+
+        rating = self.model_page.find_all('p', {'class':'rating-text'})
+        expert_rating = float(rating[0].text.split()[0])
+        user_rating = float(rating[1].text.split()[0])
+        return expert_rating, user_rating
+
+    def price_low_high(self):
+        '''extracts price range, parses it, returns the high and low price'''
+
+        price = self.model_page.find_all('p', {'class':'label-two range'})[0].text
+        price = re.sub('\$|,','', price).split()
+        low = int(price[0])
+        high = int(price[2])
+        return low, high
+
+    def expert_review(self):
+        '''returns raw text of expert review'''
+
+        model_page = self.model_page.find_all('div', {'class':'expert-review-container'})
+        expert_review = model_page[0].text
+        return expert_review
+
+    def get_reviews_link(self):
+        '''extracts the url to the reviews page'''
+
+        url_prefix = 'https://www.kbb.com'
+        url_suffix = self.model_page.find('button', {'id': 'see-all-reviews'}).get('href')
+        reviews_link = url_prefix + url_suffix
+        return self.reviews_link
+
+class ReviewsParser:
+
+    def __init__(self, reviews_link):
+        self.reviews_link = reviews_link
+
+    def find_review_count(self):
+        review_count = self.reviews_link.find('span', {'class':'js-review-pager consumer-review-page'})
+        return int(review_count.text)
+
+    def find_rating_score(self):
+        rating = self.reviews_link.find('div', {'class':'rating-number'})
+        return int(rating.text)
+
+    def extract_dates(self):
+        '''extracts string that includes date of each review'''
+
+        date_strings = []
+        for i in range(1,10,4):
+            try:
+                reviews = self.reviews_link.find_all('p', {'class':'paragraph-two'})
+                date = re.sub('\n|\t|\r', '', reviews[i].text)
+                date_strings.append(date)
+            except:
+                pass
+        return date_strings
+
+    def clean_dates(self):
+        '''extracts and cleans each of the dates on the review page'''
+
+        dates = []
+        for i in range(1,10,4):
+            try:
+                review = self.reviews_link.find_all('p', {'class':'paragraph-two'})[i].text
+                date = re.compile('\d+/\d+/\d+')
+                cleaned_date = date.findall(review)
+                dates.append(cleaned_date[0])
+            except:
+                pass
+        return dates
+
+    def extract_reviews(self):
+        '''extracts and cleans each of the (1-3) reviews on page'''
+
+        reviews = []
+        for i in range(2,11,4):
+            try:
+                review = self.reviews_link.find_all('p', {'class':'paragraph-two'})
+                cleaned_review = re.sub('\n|\t|\r', '', review[i].text)
+                reviews.append(cleaned_review)
+            except:
+                pass
+        return reviews
+
+    def extract_scores(souper):
+        '''
+        extracts and cleans each of the numberical categorical review scores
+        outputs integers as a list of lists where:
+            length of outer list = 1-3 (number of reviews)
+            length of inner list = 6 (one for each score)
+        scores: value, quality, reliability, performance, comfort, styling
+        '''
+        scores = []
+        for i in range(0,11):
+            try:
+                review = self.reviews_link.find_all('div', {'class':'col-base-6 col-md-5 text-center'})[i]
+                numbers = re.compile('\d')
+                cleaned_numbers = numbers.findall(review.text)
+                cleaned_numbers = [int(number) for number in cleaned_numbers]
+                scores.append(cleaned_numbers)
+            except:
+                pass
+        return scores
